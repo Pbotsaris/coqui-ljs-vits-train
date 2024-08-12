a946d6b2ce804f1ba35d6c542eba70264658605d
Pbotsaris <pedro@antfood.com>
2024-08-12 11:09:58 +0200

    updated setup script

--- setup.sh
+++ setup.sh
@@ -9,7 +9,8 @@ if [ -d 'vendor' ]; then
     printf "vendor directory already exists. Skipping cloning coqui-TTS\n"
  else
 printf "Cloning coqui-TTS to vendor\n"
-git clone git@github.com:Pbotsaris/coqui-TTS.git vendor
+git clone https://github.com/Pbotsaris/coqui-TTS.git vendor
+
 fi
 
 cd vendor
@@ -19,16 +20,14 @@ cd ..
 printf "Downloading dataset to $dataset_path\n"
 
 if [ -d "$dataset_path" ]; then
-    printf "Dataset directory already exists. Cleaning up...\n"
-    rm -rf $dataset_path
-    mkdir -p $dataset_path
-fi
-
+    printf "Dataset directory already exists. Skipping...\n"
+ else
 mkdir -p $dataset_path
 wget -O $dataset_path/LJSpeech-1.1.tar.bz2 https://data.keithito.com/data/speech/LJSpeech-1.1.tar.bz2 
 tar -xf $dataset_path/LJSpeech-1.1.tar.bz2 -C $dataset_path
-
 printf "Dataset downloaded and extracted to $dataset_path\n"
+fi
+
 
 printf "Setting DATASET_PATH envirioment variable\n"
 export DATASET_PATH="$dataset_path"/LJSpeech-1.1
